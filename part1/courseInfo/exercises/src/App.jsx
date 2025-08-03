b7646d314d6a74b050e07fc9c4f107674116b6b6
ravirajput99 <s.ravirajput99@gmail.com>
2025-08-03 18:44:31 +0530

    exercise - 1.6: unicafe step 1

--- part1/courseInfo/exercises/src/App.jsx
+++ part1/courseInfo/exercises/src/App.jsx
@@ -1,35 +1,25 @@
-import Content from "./component/Content"
-import Header from "./component/Header"
-import Total from "./component/Total"
+import { useState } from 'react'
+import Button from './component/Button'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-let total=0
-course.parts.forEach(part => total += part.exercises)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
   return (
     <div>
+      <h1>give feedback</h1>
       <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total total={total} />
-    </div>
+        <Button onClick={handleGood} text="good" />
+        <Button onClick={handleNeutral} text="neutral" />
+        <Button onClick={handleBad} text="bad" />
+      </div>
+      <h1>statistics</h1>
+      <p>good {good}</p><p>neutral {neutral}</p><p>bad {bad}</p>
     </div>
   )
 }
